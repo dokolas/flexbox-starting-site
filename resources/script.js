55c6d4a06a6d5bd72b133b0e1a49dd875ee65127
Spiro Dokolas <dokolas@spiros-mbp.lan>
2020-11-26 20:38:34 -0600

    Thanksgiving Fearless Leader JavaScript Added

--- resources/script.js
+++ resources/script.js
@@ -1,3 +1,28 @@
-let buttonfirst = document.getElementById("spacebutton");
+let likespace = document.getElementById("spacebutton");
+let i = 0;
 
-buttonfirst.addEventListener('click', () => document.getElementById("spacesection").style.display = "flex");
+let likespacefunc = () => {
+    i++;
+    if(i % 2 != 0){
+        document.getElementById("spacesection").style.display = "flex"; 
+        document.getElementById("spacebutton").innerHTML = "Click if you actually don't like space?";
+    }
+    else {
+        document.getElementById("spacesection").style.display = "none";
+        document.getElementById("spacebutton").innerHTML = "Click if you like space?"
+    }
+}
+
+let reyesclick = document.getElementById("reyesbutton");
+
+let reyesfunc = () => {
+    document.getElementById("scrollforleader").style.display = "flex";
+    document.getElementById("reyespic").src="./media/fearlessleader.gif";
+    document.getElementById("reyesbutton").style.display = "none";
+    document.getElementById("meme5words").innerHTML = "<h3>Blame Obama!</h3>";
+}
+
+
+
+likespace.addEventListener('click', likespacefunc);
+reyesclick.addEventListener('click', reyesfunc);
